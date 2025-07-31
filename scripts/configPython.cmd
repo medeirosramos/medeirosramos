57c51ad893c7cdc16b4fbd749ba8a7f7e7e99bcf
Rodrigo Medeiros <44650854+medeirosramos@users.noreply.github.com>
2025-07-31 18:55:04 +0000

    script de configuracao do ambiente

--- scripts/configPython.cmd
+++ scripts/configPython.cmd
@@ -2,7 +2,7 @@ REM curl -o script.cmd https://raw.githubusercontent.com/usuario/repositorio/bra
 
 @echo off
 REM setlocal
-REM set "SCRIPT_URL=https://raw.githubusercontent.com/usuario/repositorio/branch/script.cmd"
+REM set "SCRIPT_URL=https://raw.githubusercontent.com/medeirosramos/medeirosramos/refs/heads/main/scripts/configurandoAmbiente.cmd"
 REM set "TEMP_SCRIPT=%TEMP%\script.cmd"
 
 REM Este script mostra uma mensagem
