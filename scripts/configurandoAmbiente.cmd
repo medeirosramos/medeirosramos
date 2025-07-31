a65795d7f15f3db0967b30b35f74c51921487b04
Rodrigo Medeiros <44650854+medeirosramos@users.noreply.github.com>
2025-07-31 18:46:43 +0000

    script de configuracao do ambiente

---
+++ scripts/configurandoAmbiente.cmd
@@ -0,0 +1,13 @@
+REM curl -o script.cmd https://raw.githubusercontent.com/usuario/repositorio/branch/script.cmd && script.cmd && del script.cmd
+
+@echo off
+REM setlocal
+REM set "SCRIPT_URL=https://raw.githubusercontent.com/usuario/repositorio/branch/script.cmd"
+REM set "TEMP_SCRIPT=%TEMP%\script.cmd"
+
+REM Este script mostra uma mensagem
+:: Inicialização
+echo Bem-vindo!
+
+:: Fim do script
+pause
