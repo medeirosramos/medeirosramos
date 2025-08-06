c06401fa7532e8e5dc1449c5b1ca3c37cd8a8c0e
Rodrigo Medeiros <44650854+medeirosramos@users.noreply.github.com>
2025-08-06 15:54:00 +0000

    ajustando incluido criação do .flake8, .gitlab-ci.yml e testes/test_01.py

--- scripts/configPython.cmd
+++ scripts/configPython.cmd
@@ -247,25 +247,26 @@ if not exist "tests" (
 :: Verifica se o arquivo tests/test_01.py existe, se não, cria com conteúdo padrão
 if not exist "tests\test_01.py" (
     echo [INFO] Criando arquivo tests\test_01.py...
-    > tests\test_01.py (
-        echo # tests/test_01.py
-        echo import sys
-        echo import os
-        echo sys.path.insert^(0, os.path.abspath^('.'^)^)
-        echo.
-        echo import unittest
-        echo # from presos_etl_siapen.api_Siapen import test_api_siapen
-        echo.
-        echo class TestModulo1(unittest.TestCase):
-        echo     def test_padrao(self):
-        echo         # self.assertEqual(test_api_dashboard(), "resultado esperado")
-        echo         # assert test_api_siapen() == None
-        echo         assert None == None
-    )
+
+    echo # tests/test_01.py > tests\test_01.py
+    echo import sys >> tests\test_01.py
+    echo import os >> tests\test_01.py
+    echo sys.path.insert^(0, os.path.abspath^('.'^)^) >> tests\test_01.py
+    echo. >> tests\test_01.py
+    echo import unittest >> tests\test_01.py
+    echo # from presos_etl_siapen.api_Siapen import test_api_siapen >> tests\test_01.py
+    echo. >> tests\test_01.py
+    echo class TestModulo1(unittest.TestCase): >> tests\test_01.py
+    echo     def test_padrao(self): >> tests\test_01.py
+    echo         # self.assertEqual(test_api_dashboard(), "resultado esperado") >> tests\test_01.py
+    echo         # assert test_api_siapen() == None >> tests\test_01.py
+    echo         assert None == None >> tests\test_01.py
+
     echo [INFO] Arquivo test_01.py criado com sucesso.
 ) else (
     echo [INFO] Arquivo test_01.py já existe. Nenhuma ação necessária.
 )
+
 
 
 :: Cria arquivo requirements.txt se não existir
